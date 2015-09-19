5f82b286c83f5c776a2f25ebec4d50610421c0eb
tbrenneison <tori.brenneison@gmail.com>
2015-09-19 10:14:29 -0400

    Data type practices from the Learn site

---
+++ intPractice/intPractice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int five = 5;
+            int ten = 10;
+
+            int placeHolder = five;
+
+            five = ten;
+            ten = placeHolder;
+
+            Console.WriteLine(five);
+            Console.WriteLine(ten);
+
+            
+        }
+    }
+}
---
+++ stringPractice/stringPractice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = The "use" of quotations causes difficultlies.); //why are we coding something that doesn't work? This makes me itchy.
+
+            string two = "The "use" of quotations causes difficulties."); //still wrong, still itchy.
+
+            string three = "The \"use"\ "of quotations causes difficulties."); //I had to try and fix it. 
+        }
+    }
+}

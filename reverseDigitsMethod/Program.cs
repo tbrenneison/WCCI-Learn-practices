a6d464d175c076fb0f6cb12296bd273459d0a6dd
tbrenneison <tori.brenneison@gmail.com>
2015-09-24 14:00:29 -0400

    added calculator and reversed digits

---
+++ reverseDigitsMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseDigitsMethod
+{
+    class Program
+    {
+    
+        /* So this doesn't work if the reversed decimal comes back with a leading zero because 
+        it'll chop the zero off and return the actual value, but the intstructions said to return
+        a decimal.  So it does that even though I'd rather keep it as a string and just be able to 
+        reverse whatever input it was given because that is probably more applicable.  If you want
+        to reverse stuff.  In general.  Or something. */ 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a decimal number");
+            string decNum = (Console.ReadLine());
+            Console.WriteLine("the number reversed is:");
+            Console.WriteLine(reverseInput(decNum));
+           
+        }
+
+        static decimal reverseInput(string toReverse)
+        {
+            char[] letters = toReverse.ToCharArray();
+            Array.Reverse(letters);
+            string reversedString = new string(letters);
+            decimal reversedToDouble = decimal.Parse(reversedString);
+            return reversedToDouble;
+        }
+    }
+}
---
+++ simpleCalculator/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input and assign all the things
+            Console.WriteLine("Enter a number.");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Add, subtract, multiply or divide?");
+            string operation = Console.ReadLine().ToUpper();  //UPPERCASE SO THAT YOU CAN USE IT LATER NO PROBZ LOL
+            Console.WriteLine("Enter a second number.");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine(); //just for an empty space 
+
+            //okay it's time to get real 
+            if (operation == "ADD")
+            {
+                Console.WriteLine(Add(x, y));
+            }
+            else if (operation == "SUBTRACT")
+            {
+                Console.WriteLine(Subtract(x, y));
+            }
+            else if (operation == "MULTIPLY")
+            {
+                Console.WriteLine(Multiply(x, y));
+            }
+            else if (operation == "DIVIDE")
+            {
+                Console.WriteLine(Divide(x, y));
+            }
+            else
+            {
+                Console.WriteLine("ERROR: Invalid operation selected, please try again.");
+            }
+        } //END OF MAIN METHOD   do freaking not lose this stupid jerk bracket
+
+        static int Add(int x, int y)
+        {
+            int sum = x + y;
+            return sum;
+        }
+
+        static int Subtract(int x, int y)
+        {
+            int difference = x - y;
+            return difference;
+        }
+
+        static int Multiply(int x, int y)
+        {
+            int product = x * y;
+            return product;
+        }
+
+        static double Divide(double x, double y) //DOUBLES BECAUSE DECIMAL POINTS, WHOA NELLY. 
+        {
+            double dividend = x / y;
+            return dividend;
+        }
+    }
+}
